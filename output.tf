267eff4a0e456a389f04a42c464b09f155e2c784
Hibbah Ibrar <hibbah.ibrar@version1.com>
2025-05-09 15:11:59 +0100

    Exposing module outputs to Terraform Cloud

--- output.tf
+++ output.tf
@@ -1,4 +1,7 @@
-# output "instance_public_ip" {
-#   value = ""
-#   description = "The public IP address of the EC2 instance"
-# }
+output "ec2_public_ip" {
+  value = module.cacs_checklist_module.ec2_public_ip
+}
+
+output "alb_dns_name" {
+  value = module.cacs_checklist_module.alb_dns_name
+}
